6f128bf9566b0613d6122995842d581ce72f1b50
sTorov <sotorov2011@yandex.ru>
2023-06-09 00:13:48 +0300

    Адаптивная верстка для страницы поиска

--- src/components/searchPage/index.jsx
+++ src/components/searchPage/index.jsx
@@ -11,15 +11,17 @@ import background from "../../img/search-page-img.svg";
 function SearchPage(){
     return(
       <main className="main-search-page">
-        <div className="search-page-info-block">
-          <Title type="other-title">Найдите необходимые данные в пару кликов.</Title>
-          <p className="search-page__text">Задайте параметры поиска.<br/>Чем больше заполните, тем точнее поиск</p>
-          <SearchForm/>
-        </div>
-        <img className="folders-img" src={foldersImg} alt="folders-img"/>
-        <img className="document-img" src={documentImg} alt="document-img"/>
-        <div className="image-wrapper">
-          <img className="search-page-bg" src={background} alt="background-img"/>
+        <Title type="other-title">Найдите необходимые данные в пару кликов.</Title>
+        <p className="search-page__text">Задайте параметры поиска.<br/>Чем больше заполните, тем точнее поиск</p>
+        <div className="search-page-wrapper">
+          <div className="search-page-info-block">
+            <SearchForm/>
+          </div>
+          <img className="folders-img" src={foldersImg} alt="folders-img"/>
+          <img className="document-img" src={documentImg} alt="document-img"/>
+          <div className="image-wrapper">
+            <img className="search-page-bg" src={background} alt="background-img"/>
+          </div>
         </div>
       </main>
     )
